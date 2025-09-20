c862e3b644f2968e6417d301f329261229ead001
GIAN2510 <giancarlosdiazaguilar2002@gmail.com>
2025-09-20 07:36:05 -0500

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,11 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLeft = document.querySelector(".nav-left");
+
+
+  // al hacer click en el botón hamburguesa
+  hamburger.addEventListener("click", () => {
+    navLeft.classList.toggle("active");
+  });
+});
